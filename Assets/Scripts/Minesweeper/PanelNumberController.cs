fa1e295aaaeda88679690bb7c0953ce553a2f895
RyutoShimada <business.sub10110@gmail.com>
2021-05-28 19:50:43 +0900

    最初のコミット

---
+++ Assets/Scripts/Minesweeper/PanelNumberController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelNumberController : MonoBehaviour
+{
+    /// <summary>ラベルとなる UI オブジェクト</summary>
+    [SerializeField] UnityEngine.UI.Text m_label = null;
+
+    /// <summary>
+    /// 引数に指定された番号をラベルに表示する。
+    /// </summary>
+    /// <param name="number">番号</param>
+    public void SetNumber(int number)
+    {
+        if (number != 0)
+        {
+            m_label.text = number.ToString();
+        }
+    }
+}
