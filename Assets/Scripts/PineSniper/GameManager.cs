b5faf5b47bec263bfa1d5377133e0f061cddb700
RyutoShimada <business.sub10110@gmail.com>
2021-05-29 19:25:23 +0900

    アニメーションイベントで呼んで、ＵＩを表示しるようにした

--- Assets/Scripts/PineSniper/GameManager.cs
+++ Assets/Scripts/PineSniper/GameManager.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] Image _scope = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _scope.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// スコープUIを表示する
+    /// </summary>
+    public void OnScope()
+    {
+        _scope.gameObject.SetActive(true);
     }
 }
---
+++ Assets/Scripts/PineSniper/SniperRifleController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperRifleController : MonoBehaviour
+{
+    Animator _anim = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _anim.SetTrigger("AimTrigger");
+    }
+
+    /// <summary>
+    /// スコープを覗く
+    /// アニメーションイベントから呼ばれる
+    /// </summary>
+    public void DoAim()
+    {
+        FindObjectOfType<GameManager>().OnScope();
+    }
+}
