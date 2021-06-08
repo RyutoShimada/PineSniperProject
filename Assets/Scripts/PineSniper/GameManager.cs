fca93952906717985439b3394629820749b4d1fa
RyutoShimada <business.sub10110@gmail.com>
2021-06-08 20:35:35 +0900

    指定された個数の畑を生成できるようにした その畑をGeneraterMasterのListに追加し、新たなPineの配列に入れ直し、畑全体で管理するようにした。 あとは、未熟なパインの位置をランダムで変更するようにすればＯＫ そして、親オブジェクトのTransformを中心座標に来るようにする

--- Assets/Scripts/PineSniper/GameManager.cs
+++ Assets/Scripts/PineSniper/GameManager.cs
@@ -10,16 +10,28 @@ public class GameManager : MonoBehaviour
     /// <summary>初期位置にあるカメラ</summary>
     [SerializeField] GameObject _originVcam = null;
 
-    PineGenerater _pine;
+    [SerializeField] GameObject _field = null;
 
+    [SerializeField] int _fieldsWidth = 1;
+
+    [SerializeField] float _fieldSpace = 0.25f;
+
+    [SerializeField] PineGeneraterMaster _generaterMaster;
+
+    /// <summary>スコープを覗いているか</summary>
     bool _isScope;
 
+    PineGenerater _pine;
+
+    PineGeneraterMaster _master;
+    
     // Start is called before the first frame update
     void Start()
     {
-        _pine = FindObjectOfType<PineGenerater>();
+        _master = FindObjectOfType<PineGeneraterMaster>();
         _isScope = false;
         _scope.gameObject.SetActive(false);
+        CreateFields();
     }
 
     // Update is called once per frame
@@ -33,6 +45,8 @@ public class GameManager : MonoBehaviour
     /// </summary>
     public void OnScope()
     {
+        _pine = FindObjectOfType<PineGenerater>();
+        _master.Generate();
         _isScope = true;
         _scope.gameObject.SetActive(true);
         _originVcam.SetActive(false);
@@ -53,7 +67,7 @@ public class GameManager : MonoBehaviour
                 {
                     Leaf leaf = hit.collider.gameObject.GetComponent<Leaf>();
 
-                    Debug.Log($"Pine : [{_pine._pines[leaf.IndexColumn, leaf.IndexLine].gameObject.name}]");
+                    //Debug.Log($"Pine : [{_pine._pines[leaf.IndexColumn, leaf.IndexLine].gameObject.name}]");
 
                     Pine pine = _pine._pines[leaf.IndexColumn, leaf.IndexLine].gameObject.GetComponent<Pine>();
 
@@ -61,6 +75,25 @@ public class GameManager : MonoBehaviour
 
                     hit.collider.gameObject.SetActive(false);
                 }
+            }
+        }
+    }
+
+    void CreateFields()
+    {
+        float fieldWidthX = _field.transform.Find("Soil").GetComponent<Renderer>().bounds.size.x;
+        float fieldWidthZ = _field.transform.Find("Soil").GetComponent<Renderer>().bounds.size.z;
+
+        GameObject[,] fields = new GameObject[_fieldsWidth, _fieldsWidth];
+        for (int column = 0; column < _fieldsWidth; column++)
+        {
+            for (int line = 0; line < _fieldsWidth; line++)
+            {
+                fields[column, line] = Instantiate(_field);
+                Vector3 v3 = new Vector3((line * fieldWidthX) * _fieldSpace, 0, (column * fieldWidthZ) * _fieldSpace);
+                fields[column, line].gameObject.transform.position = v3;
+                _generaterMaster._generaterList.Add(fields[column, line].gameObject.transform.Find("PineGenerater").GetComponent<PineGenerater>());
+                _generaterMaster._fieldWidth = _fieldsWidth;
             }
         }
     }
---
+++ Assets/Scripts/PineSniper/PineGeneraterMaster.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PineGeneraterMaster : MonoBehaviour
+{
+    public List<PineGenerater> _generaterList = new List<PineGenerater>();
+
+    public int _fieldWidth { get; set; }
+
+    Pine[,] _pines;
+
+    public void Generate()
+    {
+        _pines = new Pine[_fieldWidth * _generaterList[0]._pines.GetLength(0), _fieldWidth * _generaterList[0]._pines.GetLength(1)];
+
+        List<Pine> pList = new List<Pine>();
+
+        int n = 0;
+
+        // 一度PineのListに入れることで、配列に入れ替える工程を楽にしている
+        for (int column = 0; column < _generaterList[0]._pines.GetLength(0); column++)
+        {
+            for (int line = 0; line < _generaterList[0]._pines.GetLength(1); line++)
+            {
+                pList.Add(_generaterList[n]._pines[column, line]);
+            }
+            n++;
+        }
+
+        // 配列に入れ替え
+        int i = 0;
+        for (int column = 0; column < _fieldWidth * _generaterList[0]._pines.GetLength(0); column++)
+        {
+            for (int line = 0; line < _fieldWidth * _generaterList[0]._pines.GetLength(1); line++)
+            {
+                _pines[column, line] = pList[i];
+                Debug.Log($"{_pines[column, line].gameObject.name}, [{column}][{line}]");
+            }
+            i++;
+        }
+    }
+}
