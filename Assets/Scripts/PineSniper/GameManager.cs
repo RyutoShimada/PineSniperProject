cb9c722e6285102a5eca4dac3246f76a187e0f11
RyutoShimada <business.sub10110@gmail.com>
2021-06-12 18:21:16 +0900

    ズームできるようにした

--- Assets/Scripts/PineSniper/GameManager.cs
+++ Assets/Scripts/PineSniper/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Cinemachine;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,8 +10,14 @@ public class GameManager : MonoBehaviour
     [SerializeField] Image _scope = null;
     /// <summary>初期位置にあるカメラ</summary>
     [SerializeField] GameObject _originVcam = null;
+    /// <summary>スコープのカメラ</summary>
+    [SerializeField] CinemachineVirtualCamera _scopeVcam = null;
+
+    [SerializeField] float _scopeDis = 5f;
 
     [SerializeField] float _pullOutDistance = 1.5f;
+
+    Vector3 _originScopePos;
 
     Generater _generater;
 
@@ -18,13 +25,18 @@ public class GameManager : MonoBehaviour
     bool _isScope;
 
     bool _isFirst = true;
-    
+
+    bool _isZoom = false;
+
+    float _originView;
+
     // Start is called before the first frame update
     void Start()
     {
         _generater = FindObjectOfType<Generater>();
         _isScope = false;
         _scope.gameObject.SetActive(false);
+        _originView = _scopeVcam.m_Lens.FieldOfView;
     }
 
     // Update is called once per frame
@@ -67,6 +79,26 @@ public class GameManager : MonoBehaviour
                     leaf._pine.PullOut(_pullOutDistance);
                     leaf.SetActivFalse();
                 }
+            }
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            float view = 0;
+
+            if (_isZoom)
+            {
+                //ズームアウト
+                view = _originView;
+                _scopeVcam.m_Lens.FieldOfView = Mathf.Clamp(value: view, min: _scopeDis, max: _originView);
+                _isZoom = false;
+            }
+            else
+            {
+                //ズームイン
+                view = _scopeVcam.m_Lens.FieldOfView - _scopeDis;
+                _scopeVcam.m_Lens.FieldOfView = Mathf.Clamp(value: view, min: _scopeDis, max: _originView);
+                _isZoom = true;
             }
         }
     }
