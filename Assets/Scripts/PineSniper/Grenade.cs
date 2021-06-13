2cacc6c7726a4df5d53d96eeead7c6e4cc350685
RyutoShimada <business.sub10110@gmail.com>
2021-06-14 00:54:00 +0900

    アセットを追加

---
+++ Assets/Scripts/PineSniper/Grenade.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    [SerializeField] GameObject _explosionParticle = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Instantiate(_explosionParticle, transform.position, transform.rotation);
+        Destroy(this.gameObject);
+    }
+}
